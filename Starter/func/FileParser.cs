5bba5b7471237447640559c74cbf07dca716727b
Daniel Bou <danielbou@outlook.com.br>
2021-02-04 17:26:35 -0300

    Projeto

---
+++ Starter/func/FileParser.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public static class FileParser
+{
+    [FunctionName("FileParser")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger("GET")] HttpRequest request)
+    {
+        string connectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
+        return new OkObjectResult(connectionString);
+    }
+}
